7a5e84629700519f83316be26cd66a5c4dd0fced
Matthew McCants <mattmccants@gmail.com>
2014-02-16 17:24:22 -0600

    Initial commit

---
+++ timemanager/views.py
@@ -0,0 +1,8 @@
+"""Views for the timemanager app."""
+# from django.views.generic import TemplateView
+
+# from . import models
+
+
+# class YourView(TemplateView):
+#    template_name = 'timemanager/default.html'
